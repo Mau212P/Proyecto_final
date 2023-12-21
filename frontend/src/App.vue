3e99dc1ee76519577ca9373801497d83f39de4b6
ErEdRacon003 <107499566+ErEdRacon003@users.noreply.github.com>
2023-12-20 23:25:36 -0600

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <AppNavbar></AppNavbar>
+    <router-view/>
+    <AppFooter></AppFooter>
+  </div>
+</template>
+
+<script>
+import AppNavbar from './components/AppNavbar.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppNavbar,
+    AppFooter
+  }
+}
+</script>
